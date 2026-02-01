68333b66815f7d2b7265233ae13dc85c57aaf535
muddassirabbasi786-sys <muddassirabbasi786@gmail.com>
2026-02-01 13:19:03 +0500

    Update handler.py

--- handler.py
+++ handler.py
@@ -3,15 +3,24 @@ import numpy as np
 import soundfile as sf
 import uuid
 
+from f5_tts.infer.utils import infer_process
+
+DEVICE = "cuda" if torch.cude.is_available() else "cpu"
+
+print("Loading F5-TTS model...")
+model = infer_process.load_model(ckpt_path=None, device=DEVICE)
+print("Model Loaded")
 def handler(event):
   text = event["input"].get("text","")
 
   if text == "":
     return {"error": "Text is required"}
 
-#fake audio for testing
-
-audio = np.zeros(24000)
+wav, sr = infer_process.infer(
+  text=text,
+  model=model,
+  device=DEVICE
+)
 
 filename = f"tmp/
 {uuid.uuid4()}.wav"
